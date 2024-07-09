90053b6b7ec69fe10598cea96df1326338967628
tmasthay <tyler@ices.utexas.edu>
2024-07-09 02:58:36 -0500

    DEVOPS: Fix RSFROOT sourcing

--- .github/workflows/helpers/ci/generate_docs.sh
+++ .github/workflows/helpers/ci/generate_docs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 source $SCRIPT_PATH/env.sh
+source $MAD_ENV_PATH
 
+echo "RSFROOT=$RSFROOT"
 julia --project=docs -e 'using Pkg; Pkg.develop(path=pwd()); Pkg.instantiate()'
 julia --project=docs docs/make.jl

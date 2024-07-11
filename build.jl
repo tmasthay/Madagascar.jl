bbf836fcd9e50a62f56f173cd277c323c6f4d8eb
tmasthay <tyler@ices.utexas.edu>
2024-07-11 15:28:14 -0500

    BUILD: Move to custom build.jl

---
+++ build.jl
@@ -0,0 +1,16 @@
+using Pkg
+
+Pkg.add("Libdl")
+if Sys.islinux()
+    println("Linux detected: Adding Madagascar_jll dependency.")
+    Pkg.add("Madagascar_jll")
+elseif Sys.isapple()
+    println("macOS detected: Madagascar_jll is not available for macOS.")
+    println("Please build Madagascar from source if you wish to use it.")
+elseif Sys.iswindows()
+    println("Windows detected: Madagascar_jll is not available for Windows.")
+    println("Please build Madagascar from source if you wish to use it.")
+else
+    println("Unsupported operating system detected.")
+    println("Please build Madagascar from source if you wish to use it.")
+end
